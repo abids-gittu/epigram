10c793e48d566e3da4d7a512abdf8cd909fe52b4
abidscode <mh.abid.2003@gmail.com>
2025-12-04 14:54:43 +0600

    done with pages

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,13 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
-
-
+import RTE from "./RTE";
+import Signup from "./Signup"
 export { 
     Header,
     Footer,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup
 };
